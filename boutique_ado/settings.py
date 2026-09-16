d4bb49b007b2efc95a3aaad8c0a158efdb972cd7
Neall Dexter <neall.t.dexter@gmail.com>
2026-09-16 14:06:25 +0100

    completed homepage header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -141,7 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
